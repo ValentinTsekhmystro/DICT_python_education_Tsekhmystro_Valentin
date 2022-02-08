6901d40bb4c1e96f95a95abebf4e233d89424ced
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-08 15:59:50 +0200

    CoffeeMachine 4-st stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,14 +1,48 @@
-water = 200
-milk = 50
-coffee = 15
-has_water = int(input("Write how many ml of water the coffee machine has:\n"))
-has_milk = int(input("Write how many ml of milk the coffee machine has:\n"))
-has_coffee = int(input("Write how many grams of coffee beans the coffee machine has:\n"))
-need_cup_coffee = int(input("Write how many cups of coffee you will need:\n"))
-cup_coffee = min([has_water // water, has_milk // milk, has_coffee // coffee])
-if cup_coffee == need_cup_coffee:
-    print("Yes, I can make that amount of coffee")
-elif cup_coffee > need_cup_coffee:
-    print(f'Yes, I can make that amount of coffee (and even {cup_coffee - need_cup_coffee} morethan that)')
-else:
-    print(f"No, I can make only {cup_coffee} cups of coffee")
+water = 400
+milk = 540
+coffee = 120
+money = 550
+cups = 9
+print("The coffee machine has:")
+print(f"{water} of water")
+print(f"{milk} of milk")
+print(f"{coffee} of coffee")
+print(f"{cups} of disposable cups")
+print(f"{money} of money")
+action = input("Write action (buy, fill, take):\n")
+if action == "buy":
+    type_of_coffee = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"))
+    if type_of_coffee == 1:
+        if water >= 250 and coffee >= 16 and cups >= 1:
+            water -= 250
+            coffee -= 16
+            cups -= 1
+            money += 4
+    elif type_of_coffee == 2:
+        if water >= 350 and milk >= 75 and coffee >= 20 and cups >= 1:
+            water -= 350
+            coffee -= 20
+            milk -= 75
+            cups -= 1
+            money += 7
+    elif type_of_coffee == 3:
+        if water >= 200 and milk >= 100 and coffee >= 10 and cups >= 1:
+            water -= 200
+            coffee -= 10
+            milk -= 100
+            cups -= 1
+            money += 6
+elif action == "fill":
+    water += int(input("Write how many ml of water you want to add:\n"))
+    milk += int(input("Write how many ml of milk you want to add:\n"))
+    coffee += int(input("Write how many grams of coffee beans you want to add:\n"))
+    cups += int(input("Write how many disposable coffee cups you want to add:\n"))
+elif action == "take":
+    print(f"I gave you {money}")
+    money = 0
+print("The coffee machine has:")
+print(f"{water} of water")
+print(f"{milk} of milk")
+print(f"{coffee} of coffee")
+print(f"{cups} of disposable cups")
+print(f"{money} of money")
