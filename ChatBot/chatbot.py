6be0f23fd0004f0185ab493893f916e3a618b9ab
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-08 15:33:18 +0200

    ChatBot 5-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -10,4 +10,16 @@ print("Your age is", age, "that's a good time to start programming!")
 a = int(input("Now I will prove to you that I can count to any number you want.\n"))
 for i in range(a +1):
     print(str(i) + "!")
-print("Completed, have a nice day!")
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("""1. To repeat a statement multiple times.
+2. To decompose a program into several small subroutines.
+3. To determine the execution time of a program.
+4. To interrupt the execution of a program.""")
+while True:
+    k = int(input())
+    if k == 2:
+        print("Completed, have a nice day!\nCongratulations, have a nice day!")
+        break
+    else:
+        print("Please, try again.")
