e56c4e0aa8c316c3349cbdc5c587f478c9038f19
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-08 15:51:50 +0200

    Hangman 7-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,26 +4,31 @@ print("HANGMAN")
 my_list = ("python", "java", "javascript", "php")
 my_word = random.choice(my_list)
 remember_letters =[]
+input_letters = []
 all_letters = list(set(my_word))
 word = "".join([i if i in remember_letters else "_" for i in my_word])
 print(word)
 life = 8
 while life > 0:
     letter = input("Input a letter:")
-    if letter in my_word and letter not in remember_letters:
-        remember_letters.append(letter)
-        all_letters.remove(letter)
-    elif letter in remember_letters:
-        print("No improvements")
-        life -= 1
-    else:
-        print("That letter doesn't appear in the word")
-        life -= 1
-    word = "".join([i if i in remember_letters else "_" for i in my_word])
-    print(word)
-    if len(all_letters) == 0:
-        print("You guessed the word!\nYou survived!")
-        break
+    if letter == letter.lower() and len(letter) == 1:
+        if letter in my_word and letter not in remember_letters and letter not in input_letters:
+            remember_letters.append(letter)
+            all_letters.remove(letter)
+        elif letter in remember_letters or letter in input_letters:
+            print("You've already guessed this letter")
+        else:
+            print("That letter doesn't appear in the word")
+            life -= 1
+        word = "".join([i if i in remember_letters else "_" for i in my_word])
+        print(word)
+        if len(all_letters) == 0:
+            print("You guessed the word!\nYou survived!")
+            break
+    elif letter != letter.lower():
+        print("Please enter a lowercase English letter")
+    elif len(letter) != 1:
+        print("You should input a single letter")
 
 if len(all_letters) > 0:
     print("You lost!")
