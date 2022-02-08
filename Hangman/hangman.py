05e3c537c48829f40d37e99ca23a7d63d18489de
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-08 15:45:59 +0200

    Hangman 4-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,8 +3,9 @@ import random
 
 print("HANGMAN")
 my_list = ("python", "java", "javascript", "php")
-a = input("Guess the word:")
-if a == random.choice(my_list):
+my_word = random.choice(my_list)
+a = input("Guess the word " + my_word[:3] + "_".join(['' for _ in range(len(my_word)-3)]) + ":")
+if a == my_word:
     print("You survived!")
 else:
     print("You lost!")
