453ab088216e9b70d654157f561fe2690a906372
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-08 15:49:22 +0200

    Hangman 6-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,5 @@
 import random
 
-
 print("HANGMAN")
 my_list = ("python", "java", "javascript", "php")
 my_word = random.choice(my_list)
@@ -14,11 +13,17 @@ while life > 0:
     if letter in my_word and letter not in remember_letters:
         remember_letters.append(letter)
         all_letters.remove(letter)
+    elif letter in remember_letters:
+        print("No improvements")
+        life -= 1
     else:
+        print("That letter doesn't appear in the word")
         life -= 1
     word = "".join([i if i in remember_letters else "_" for i in my_word])
     print(word)
     if len(all_letters) == 0:
+        print("You guessed the word!\nYou survived!")
         break
 
-print("Thanks for playing!\nWe'll see how well you did in the next stage")
+if len(all_letters) > 0:
+    print("You lost!")
