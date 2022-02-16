1707e565593283e99fa5a344d7df2b28772fbaaa
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-16 14:55:03 +0200

    TicTacToe 3-st stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,9 +1,29 @@
-line = input("Enter cells: ")
+user_inf = input("Enter cells: ")
+
+row_list = [[item for item in user_inf[i:i + 3]] for i in range(0, 7, 3)]
+col_list = [[item[i] for item in row_list] for i in range(3)]
+main_diag = [[row_list[i][i] for i in range(3)]]
+sec_diag = [[row_list[i][2 - i] for i in range(3)]]
+
+game_matrix = row_list + col_list + main_diag + sec_diag
 print("---------")
-for n,i in enumerate(line):
-    if n % 3 == 0:
-        print('| ', end="")
-    print(i, end= ' ')
-    if n % 3 == 2:
-        print('|')
+print("|", *row_list[0], "|")
+print("|", *row_list[1], "|")
+print("|", *row_list[2], "|")
 print("---------")
+
+X_NUMS = user_inf.count("X")
+O_NUMS = user_inf.count("O")
+win_x = ["X", "X", "X"]
+win_o = ["O", "O", "O"]
+
+if (win_o in game_matrix and win_x in game_matrix) or abs(X_NUMS - O_NUMS) > 1:
+    print("Impossible")
+elif win_o in game_matrix:
+    print("O wins")
+elif win_x in game_matrix:
+    print("X wins")
+elif "_" in user_inf:
+    print("Game not finished")
+else:
+    print("Draw")
