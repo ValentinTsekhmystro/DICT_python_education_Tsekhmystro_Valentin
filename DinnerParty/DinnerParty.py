39c7bc6a00f94de73f2a4e5073fad0b957f28ccb
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-20 12:29:45 +0200

    DinnerParty 2-st stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,16 +1,16 @@
-number_of_friends = int(input())
-print('Enter the number of friends joining (including you):')
-
+number_of_friends = int(input('Enter the number of friends joining (including you):\n'))
 
 if number_of_friends < 1:
     print('No one is joining for the party')
     exit()
-
 payments = {}
 print('Enter the name of every friend (including you), each on a new line:')
-
 for _ in range(number_of_friends):
     name = input()
     payments[name] = 0
 
+total_amount = int(input("Enter the total amount:\n"))
+a = round(total_amount / number_of_friends, 2)
+for name in payments:
+    payments[name] = a
 print(payments)
