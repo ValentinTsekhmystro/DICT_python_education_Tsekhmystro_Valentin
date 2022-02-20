ecc06131e0b69be3e1473b871e29411ea6e9a2ac
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-02-20 12:17:57 +0200

    CreditCalculator 2-st stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,5 +1,16 @@
-print("""Loan principal: 1000
-Month 1: repaid 250
-Month 2: repaid 250
-Month 3: repaid 500
-The loan has been repaid!""")
+credit_sum = int(input("Enter the loan principal:\n"))
+credit = input("""What do you want to calculate?
+type "m" - for number of monthly payments,
+type "p" - for the monthly payment:\n""")
+if credit in "m":
+    m = int(input("Enter the monthly payment:\n"))
+    a = credit_sum // m
+    print(f"It will take {a} months to repay the loan")
+elif credit in "p":
+    p = int(input("Enter the number of months:\n"))
+    b = credit_sum // p + 1
+    c = credit_sum - (p - 1) * b
+    if b == c:
+        print(f"Your monthly payment = {b}")
+    else:
+        print(f"Your monthly payment = {b} and the last payment = {c}")
