4165cfcf37e51f3d25cbb3a6a3a0c4d6f5503e59
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-04-21 12:16:39 +0300

    pock_paper_scisors 5-st stage{fix}

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,45 +1,45 @@
 import random
 
-name = input("Enter your name: ")
-print("Hello, " + name)
 
-	@@ -11,25 +8,39 @@
-with open('rating.txt') as infile:
-    for line in infile.readlines():
-        line_list = line.split()
-        if line_list[0] == name:
-            score = int(line_list[1])
-            break
-options = "rock,paper,scissors".split(',')
-options_input = input()
-if len(options_input) > 0:
-    options = options_input.split(',')
-
+random.seed()
+default_options = ('rock', 'paper', 'scissors')
+default_beat_options = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+user_name = input('Enter your name: ')
+score = 0
+with open('rating.txt', 'r') as f:
+    for line in f:
+        if line.startswith(user_name):
+            score = int(line.replace(user_name + ' ', ''))
+print(f'Hello, {user_name}!')
+options = list()
+beat_options = dict()
+list_of_options = input()
+if list_of_options != '':
+    options = list_of_options.split(',')
+    beat_options = dict.fromkeys(options)
+    for index, option in enumerate(options):
+        new_list = options[index + 1:] + options[:index]
+        new_list = new_list[:len(new_list) // 2]
+        beat_options[option] = new_list
+else:
+    options = default_options
+    beat_options = default_beat_options
 print("Okay, let's start")
-
-while True:
-    computer = options[random.randint(0, len(options) - 1)]
-    user = input()
-
-    if user == "!exit":
-        print("Bye!")
-        break
-    if user == "!rating":
-        print("Your rating: {}".format(score))
-        continue
-    if user not in options:
-        print("Invalid input")
-        continue
-    comp_idx = options.index(computer)
-    user_idx = options.index(user)
-    if user_idx > comp_idx:
-        user_idx -= len(options)
-
-    if user == computer:
-        print("There is a draw (" + computer + ")")
-        score += 50
-    elif comp_idx - user_idx <= len(options) // 2:
-        print("Sorry, but computer chose " + computer)
+user_input = input()
+while user_input != '!exit':
+    if user_input == '!rating':
+        print('Your rating:', score)
+    elif user_input in options:
+        option = random.choice(options)
+        if option == user_input:
+            score += 50
+            print(f'There is a draw ({option})')
+        elif option in beat_options[user_input]:
+            print(f'Sorry, but the computer chose {option}')
+        elif user_input in beat_options[option]:
+            score += 100
+            print(f'Well done. The computer chose {option} and failed')
     else:
-        print("Well done. Computer chose " + computer + " and failed")
-        score += 100
+        print('Invalid input')
+    user_input = input()
+print('Bye!')
