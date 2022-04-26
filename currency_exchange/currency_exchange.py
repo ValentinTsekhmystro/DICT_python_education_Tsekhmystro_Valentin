a0a987bcb89d81e11a7f77c98d1ced4dfe6e791b
ValentinTsekhmystro <v.v.tsekhmystro@student.khai.edu>
2022-04-26 13:31:58 +0300

    currency_exchange 2-st stage

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,3 +1,6 @@
-amount = int(input('Please, enter the number of conicoins you have: '))
-xchange_rate = float(input('Please, enter the exchange rate: '))
-print(f'The total amount of dollars: {amount * xchange_rate}')
+amount = float(input())
+xchange_rates = [0.82, 0.17, 1.9622, 0.208]
+print(f'I will get {round(amount * xchange_rates[0], 2)} ARS from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[1], 2)} HNL from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[2], 2)} AUD from the sale of {amount} conicoins.')
+print(f'I will get {round(amount * xchange_rates[3], 2)} MAD from the sale of {amount} conicoins.')
